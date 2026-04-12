d5db5b6d239069c412dfe1ab07a47959562fdf63
Ahmad <ahmad477835@gmail.com>
2026-04-12 18:28:18 +0200

    Fix a small bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 import org.gradle.kotlin.dsl.application
-import sun.jvmstat.monitor.MonitoredVmUtil.mainClass
 
 plugins {
     kotlin("jvm") version "1.9.25"
     application
-    id("edu.udo.cs.sopra") version "1.0.3"
+
 }
 
 group = "edu.udo.cs.sopra"
