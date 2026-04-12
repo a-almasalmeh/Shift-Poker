75b106b984830fc8b6c98cddbeb4b7cb715d2d6c
Ahmad <ahmad477835@gmail.com>
2026-04-12 18:25:10 +0200

    Add some configuration and the Main.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,31 @@
+import org.gradle.kotlin.dsl.application
+import sun.jvmstat.monitor.MonitoredVmUtil.mainClass
+
 plugins {
-    kotlin("jvm") version "2.3.0"
+    kotlin("jvm") version "1.9.25"
+    application
+    id("edu.udo.cs.sopra") version "1.0.3"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
+group = "edu.udo.cs.sopra"
+version = "1.0"
 
 /* Change this to the version of the BGW you want to use */
 val bgwVersion = "0.10"
 
+kotlin {
+    jvmToolchain(11)
+}
+
+
 repositories {
     mavenCentral()
 }
+
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
     implementation(group = "tools.aqua", name = "bgw-gui", version = bgwVersion)
     implementation(group = "tools.aqua", name = "bgw-net-common", version = bgwVersion)
     implementation(group = "tools.aqua", name = "bgw-net-client", version = bgwVersion)
-}
-
-kotlin {
-    jvmToolchain(11)
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+import gui.ShiftPokerApplication
+
+/**
+ * Main entry point that starts the [ShiftPokerApplication]
+ *
+ * Once the application is closed, it prints a message indicating the end of the application.
+ */
+fun main() {
+    ShiftPokerApplication().show()
+    println("Application ended. Goodbye")
+}
